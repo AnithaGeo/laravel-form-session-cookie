b7188055fb13dfb165d46d20a72564d1cc3036c2
Anitha George <anithageorge101@gmail.com>
2023-09-19 22:08:15 +0530

    first commit

---
+++ routes/web.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function (Request $request) {
+    Session::put("message","logged in succesfully");
+    $request->session()->flash("hello","hello world");
+    Cookie::queue("hello","testing",10);
+    // $request->session()->forget('message');
+    dd(Cookie::get("hello"));
+    dd($request->session()->all());
+
+    return view('welcome');
+});
+
+Route::get('/about', function (Request $request) {
+    // Session::put("message","logged in succesfully");
+    // $request->session()->flash("hello","hello world");
+    // $request->session()->forget('message');
+
+    dd($request->session()->all());
+
+    return view('welcome');
+});
+
+Route::get('/form', function () {
+    return view('form');
+})->name('form');
+
+Route::post("post",function(Request $request) {
+    $validated=$request->validate([
+        'name'=>'required',
+        'age'=>['required','gte:18'],
+        'date'=>['required','date','before_or_equal:tomorrow'],
+        'image'=>['required','image','mimes:png']
+        // 'date'=>['required','date','after:tomorrow']
+    ]);
+    if($request->file("image")){
+        foreach($request->file('image')as $file){
+            $filename=date("YmdHi").$file->getClientOriginalName();
+        }
+    }
+})->name("submit.form");
+
